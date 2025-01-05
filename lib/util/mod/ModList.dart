9cb5b8a043332d9c5b05f08944534ac5d853e31d
TaYaKi71751 <shantian.tw@gmail.com>
2025-01-06 00:06:39 +0900

    Add entity3

--- lib/util/mod/ModList.dart
+++ lib/util/mod/ModList.dart
@@ -14,9 +14,16 @@ class ModList {
       }
 						List<String> dpath = entity.path.split('/');
 						dpath = (dpath.join('/').replaceAll('/Disabled', '').replaceAll(Directory.current.path, Directory.current.path + '/Disabled')).split('/');
-						print(dpath.join('/'));
 						if(Directory(dpath.join('/')).existsSync()){
+							print(dpath);
 							 for (FileSystemEntity entity2 in Directory(dpath.join('/')).listSync()) {
+									print(entity2.path);
+									for (FileSystemEntity entity3 in Directory(entity2.path).listSync()) {
+										var m = Mod(entity3.path);
+										if (m.isMod == true) {
+											rtn.add(m);
+										}
+									}
 									var m = Mod(entity2.path);
 									if (m.isMod == true) {
 										rtn.add(m);
@@ -24,12 +31,24 @@ class ModList {
 								}
 						}
       if (Directory(entity.path).existsSync()) {
+							try {
         for (FileSystemEntity entity2 in Directory(entity.path).listSync()) {
+									try {
+									 for(FileSystemEntity entity3 in Directory(entity2.path).listSync()){
+          var m = Mod(entity3.path);
+          if (m.isMod == true) {
+            rtn.add(m);
+          }
+
+										}
+									} catch (e){}
           var m = Mod(entity2.path);
           if (m.isMod == true) {
             rtn.add(m);
           }
+									
         }
+							}catch(e){}
       }
     }
     return rtn;
