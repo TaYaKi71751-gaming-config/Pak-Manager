a0f739846ce0bba604eb2b5e48f2340446b8301a
TaYaKi71751 <shantian.tw@gmail.com>
2025-01-05 17:09:35 +0900

    Fix process name

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Future<void> _kill() async {
-    await Task.kill('Palworld.exe');
+    await Task.kill('Palworld-Win64-Shippiing.exe');
   }
 
   @override
