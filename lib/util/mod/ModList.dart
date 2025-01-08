9227eb2b8b3df360d2eec54e6e70b979bbe880cf
TaYaKi71751 <shantian.tw@gmail.com>
2025-01-08 18:51:49 +0900

    Add try-catch

--- lib/util/mod/ModList.dart
+++ lib/util/mod/ModList.dart
@@ -6,49 +6,55 @@ class ModList {
   static List<Mod> get all {
     List<Mod> rtn = List.empty(growable: true);
     for (FileSystemEntity entity in Directory.current.listSync()) {
-      if (File(entity.path).existsSync()) {
-        var m = Mod(entity.path);
-        if (m.isMod == true) {
-          rtn.add(m);
-        }
-      }
-						List<String> dpath = entity.path.split(Platform.pathSeparator);
-						dpath = (dpath.join(Platform.pathSeparator).replaceAll(Platform.pathSeparator + 'Disabled', '').replaceAll(Directory.current.path, Directory.current.path + Platform.pathSeparator + 'Disabled')).split(Platform.pathSeparator);
-						if(Directory(dpath.join(Platform.pathSeparator)).existsSync()){
-							print(dpath);
-							 for (FileSystemEntity entity2 in Directory(dpath.join(Platform.pathSeparator)).listSync()) {
-									print(entity2.path);
-									for (FileSystemEntity entity3 in Directory(entity2.path).listSync()) {
-										var m = Mod(entity3.path);
-										if (m.isMod == true) {
-											rtn.add(m);
-										}
-									}
-									var m = Mod(entity2.path);
-									if (m.isMod == true) {
-										rtn.add(m);
-									}
-								}
-						}
-      if (Directory(entity.path).existsSync()) {
-							try {
-        for (FileSystemEntity entity2 in Directory(entity.path).listSync()) {
-									try {
-									 for(FileSystemEntity entity3 in Directory(entity2.path).listSync()){
-          var m = Mod(entity3.path);
+      try {
+        if (File(entity.path).existsSync()) {
+          var m = Mod(entity.path);
           if (m.isMod == true) {
             rtn.add(m);
           }
-
-										}
-									} catch (e){}
+        }
+      } catch (e) {}
+      List<String> dpath = entity.path.split(Platform.pathSeparator);
+      dpath = (dpath
+              .join(Platform.pathSeparator)
+              .replaceAll(Platform.pathSeparator + 'Disabled', '')
+              .replaceAll(Directory.current.path,
+                  Directory.current.path + Platform.pathSeparator + 'Disabled'))
+          .split(Platform.pathSeparator);
+      if (Directory(dpath.join(Platform.pathSeparator)).existsSync()) {
+        for (FileSystemEntity entity2
+            in Directory(dpath.join(Platform.pathSeparator)).listSync()) {
+          try {
+            for (FileSystemEntity entity3
+                in Directory(entity2.path).listSync()) {
+              var m = Mod(entity3.path);
+              if (m.isMod == true) {
+                rtn.add(m);
+              }
+            }
+          } catch (e) {}
           var m = Mod(entity2.path);
           if (m.isMod == true) {
             rtn.add(m);
           }
-									
         }
-							}catch(e){}
+      }
+      if (Directory(entity.path).existsSync()) {
+        for (FileSystemEntity entity2 in Directory(entity.path).listSync()) {
+          try {
+            for (FileSystemEntity entity3
+                in Directory(entity2.path).listSync()) {
+              var m = Mod(entity3.path);
+              if (m.isMod == true) {
+                rtn.add(m);
+              }
+            }
+          } catch (e) {}
+          var m = Mod(entity2.path);
+          if (m.isMod == true) {
+            rtn.add(m);
+          }
+        }
       }
     }
     return rtn;
